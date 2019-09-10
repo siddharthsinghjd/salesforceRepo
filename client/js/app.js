1b9340d8e6651293a1db042ecc7c3e350a889e4e
siddharthsinghjd <39839817+siddharthsinghjd@users.noreply.github.com>
2019-09-10 23:39:03 +0530

    update app.js to change the namespace of org

--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,15 @@
 function getSessionList(success, error) {
-  var soql = "SELECT Session__r.Id, Session__r.Name FROM Session_Speaker__c";
+  var soql = "SELECT sid16__Session__r.Id, sid16__Session__r.Name FROM sid16__Session_Speaker__c";
   force.query(soql, success, error);
 }
 
 function getSessionDetails(sessionId, success, error) {
-  var soql = "SELECT Session__r.Name, " +
-  "Session__r.Session_Date__c, " +
-  "Speaker__r.First_Name__c, " +
-  "Speaker__r.Last_Name__c " +
-  "FROM Session_Speaker__c " +
-  "WHERE Session__r.Id = '" + sessionId + "'";
+  var soql = "SELECT sid16__Session__r.Name, " +
+  "sid16__Session__r.Session_Date__c, " +
+  "sid16__Speaker__r.First_Name__c, " +
+  "sid16__Speaker__r.Last_Name__c " +
+  "FROM sid16__Session_Speaker__c " +
+  "WHERE sid16__Session__r.Id = '" + sessionId + "'";
   force.query(soql, success, error);
 }
 
@@ -19,7 +19,7 @@ function showSessionList() {
             var sessions = data.records,
                 html = '';
             for (var i=0; i<sessions.length; i++) {
-                html += '<li class="table-view-cell"><a href="#sessions/'+ sessions[i].Session__r.Id +'">' + sessions[i].Session__r.Name + '</a></li>';
+                html += '<li class="table-view-cell"><a href="#sessions/'+ sessions[i].sid16__Session__r.Id +'">' + sessions[i].sid16__Session__r.Name + '</a></li>';
             }
             html =
                 '<div class="page">' +
@@ -53,14 +53,14 @@ function showSessionDetails(sessionId) {
                     '<div class="card">' +
                         '<ul class="table-view">' +
                             '<li class="table-view-cell">' +
-                                '<h4>' + session.Session__r.Name + '</h4>' +
-                                '<p>' + (session.Session__r.Session_Date__c || 'No time yet')+ '</p>' +
+                                '<h4>' + session.sid16__Session__r.Name + '</h4>' +
+                                '<p>' + (session.sid16__Session__r.Session_Date__c || 'No time yet')+ '</p>' +
                             '</li>' +
                             '<li class="table-view-cell">Speaker: ' +
-                                session.Speaker__r.First_Name__c +
+                                session.sid16__Speaker__r.First_Name__c +
                             '</li>' +
                             '<li class="table-view-cell">' +
-                                (session.Session__r.Description__c || 'No description yet') +
+                                (session.sid16__Session__r.Description__c || 'No description yet') +
                             '</li>' +
                         '</ul>' +
                     '</div>' +
